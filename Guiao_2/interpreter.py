afa57cf38f50191a930d5c80835239a84e1f3006
Elstryker <rubencorreiacerqueira@gmail.com>
2022-03-29 15:36:05 +0100

    Commenting

--- Guiao_2/interpreter.py
+++ Guiao_2/interpreter.py
@@ -34,6 +34,7 @@ class MyInterpreter (Interpreter):
     
     def start(self,tree):
         r = []
+        # Visita todos os filhos em que cada um vão retornar o seu código
         for child in tree.children:
             r += self.visit(child)
 
@@ -42,6 +43,7 @@ class MyInterpreter (Interpreter):
         (warnings, errors) = detectWarningsAndErrors(self.variables)
 
         output = dict()
+        # Juntar o código dos vários blocos
         output["code"] = "\n".join(r)
         output["numberIfs"] = self.numberIfs
         output["warnings"] = warnings
@@ -52,6 +54,7 @@ class MyInterpreter (Interpreter):
     
     def initializations(self,tree):
         r = []
+        # Busca do código das inicializações
         for child in tree.children:
             if isinstance(child,Tree):
                 r.append(self.visit(child))
@@ -64,13 +67,14 @@ class MyInterpreter (Interpreter):
         type = str(tree.children[0])
 
         varName = self.visit(tree.children[1])
+        # Atualiza a estrutura com o estado das variáveis
         if varName not in self.variables:
             self.variables[varName] = [True,False,False]  # Declared, Assigned, Used
         else:
             self.variables[varName][0] = True
 
         code = f"{type} {varName}"
-
+        # Caso a declaração tenha inicialização, processa-a
         if childNum > 2:
             value = self.visit(tree.children[3])
 
@@ -88,6 +92,7 @@ class MyInterpreter (Interpreter):
 
     def code(self,tree):
         r = list()
+        # Busca do código das inicializações
         for child in tree.children:
             r.append(self.visit(child))
 
@@ -101,6 +106,7 @@ class MyInterpreter (Interpreter):
 
     def operand(self,tree):
         value = self.visit(tree.children[0])
+        # Atualização do estado da variável na estrutura de dados caso o operando seja variavel
         if tree.children[0].data == 'var':
             if value in self.variables:
                 self.variables[value][2] = True
@@ -111,7 +117,7 @@ class MyInterpreter (Interpreter):
 
     def atribution(self,tree):
         varName = self.visit(tree.children[0])
-
+        # Atualização do estado da variável na estrutura de dados
         if varName in self.variables:
             self.variables[varName][1] = True
         else:
@@ -124,6 +130,7 @@ class MyInterpreter (Interpreter):
 
     def condition(self,tree):
         self.numberIfs += 1
+        # Variável local para guardar o seu nível
         ifDepth = self.ifDepth
         self.ifDepth += 1
         self.maxIfDepth = self.maxIfDepth if self.maxIfDepth > ifDepth else ifDepth
@@ -134,6 +141,7 @@ class MyInterpreter (Interpreter):
 
         codeFalse = ''
 
+        # Cálculo da identação para pretty printing
         ident = ((ifDepth + 1) * identLevel * " ")
         lastIdent = (ifDepth * identLevel * " ")
 
@@ -142,6 +150,7 @@ class MyInterpreter (Interpreter):
 
         codeString = "if" + boolexpr + """ { // level """ + str(ifDepth) + "\n" + ident + ("\n"+ident).join(codeTrue) + "\n" + lastIdent + "}"
         
+        # Processamento de else se houver
         if codeFalse != '':
             codeString += "\n"+ lastIdent + "else { // level " + str(ifDepth) + "\n" + ident + ("\n"+ident).join(codeFalse) + "\n" + lastIdent + "}"
 
