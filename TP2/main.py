3d269d6dd5e201c61f87487f7b40fae51e190e1b
Rúben Cerqueira <61189746+Elstryker@users.noreply.github.com>
2022-04-12 22:30:07 +0100

    Rc/DeclarationVisitors (#1)

--- TP2/main.py
+++ TP2/main.py
@@ -8,42 +8,25 @@ from interpreter.interpreter import MainInterpreter
 phrase = """
 str a = "1";
 int b;
+str b = a;
 
 int set c;
-int tuple a = (1.2,2.,3.);
+str set guy = {"on","tw","th","fo"};
+float tuple tu = (1.2,2.,3.);
 (str,int) dict di = {3:"ro",6:"he"};
 bool goo = False;
 
 a = 1;
 
-if(cond > 0 + 1 && a + 1 == 0 || cond < 2) {
-    if(y[0] > 1) {
-        if(z > 2) {
-            z = 1; k = 3;
-        }
-        if(k > 2) {
-            k=2; z=9; x=5;
-        }
-    }
-    while(cond > 0) {
-        if(z < 8) {
-            z = 1;
-        }
-        else {
-            x[1] = 0;
-        }
-    }
-}
-
 x=3;
 """
-
-print(grammar)
 
 p = Lark(grammar)
 
 parse_tree = p.parse(phrase)
 
-# data = MainInterpreter().visit(parse_tree)
+data = MainInterpreter().visit(parse_tree)
 
-print(parse_tree.pretty())
+print(data)
+
+# print(parse_tree.pretty())
