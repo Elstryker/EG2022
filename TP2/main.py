aa6e6fa45415dd6a03d6ac85c4a1e2519d09cd96
Elstryker <rubencorreiacerqueira@gmail.com>
2022-04-13 16:40:47 +0100

    Declaration visitors finished + exercise 2 done

--- TP2/main.py
+++ TP2/main.py
@@ -7,13 +7,15 @@ from interpreter.interpreter import MainInterpreter
 
 phrase = """
 str a = "1";
+
+str b = a;
 int b;
-str b = 3.;
 
 int set c;
 str set guy = {"on","tw","th","fo"};
 float tuple tu = (1.2,2.,3.);
-bool goo = False;
+(int,str) dict di = {3:"ro",6:"he",7:"ko",9.:"o"};
+bool goo = 2;
 
 a = 1;
 
@@ -26,6 +28,10 @@ parse_tree = p.parse(phrase)
 
 data = MainInterpreter().visit(parse_tree)
 
-print(data)
+print(data["html"])
+
+import json
+
+print(json.dumps(data['vars'],sort_keys=True, indent=4))
 
 # print(parse_tree.pretty())
